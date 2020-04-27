bb25545df45ad3d6f8d6aa7dde91e7ffe3e086a8
chengxq <chengxq513@gmail.com>
2020-04-27 20:57:08 +0800

    shop-input

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
     <div id="app" :data-theme="themeName + '-theme'">
         <router-view />
-        <shop-input v-model="test" @change="inputtest" bordered></shop-input>
+        <shop-input v-model="test" placeholder="test" bordered :rounded="false" validate :rules="rules">
+        </shop-input>
+        <div style="width:200px;height:300px;border-radius:50%;overflow:hidden;">
+            <img style="" src="https://image.ibb2b.com/productcontent/2020/04/15/01e4072e-acfb-43c4-97c7-59c03ee8f0c2.jpg@0r_85Q" />
+        </div>
     </div>
 </template>
 
@@ -10,7 +14,11 @@ export default {
     data(){
         return{
             btnLoading:false,
-            test:'888'
+            test:'888',
+            rules:[
+                {required:true, message:'test'},
+                {leng:6,message:'test2'}
+            ]
         }
     },
     computed:{
