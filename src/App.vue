cbfa5da852cff44a2dfa04125138cb67adce580a
pijunkuan@gmail.com <pijunkuan@gmail.com>
2020-04-27 22:05:37 +0800

    img

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,7 @@
         <router-view />
         <shop-input v-model="test" placeholder="test" bordered :rounded="false" validate :rules="rules">
         </shop-input>
-        <div style="width:200px;height:300px;border-radius:50%;overflow:hidden;">
-            <img style="" src="https://image.ibb2b.com/productcontent/2020/04/15/01e4072e-acfb-43c4-97c7-59c03ee8f0c2.jpg@0r_85Q" />
-        </div>
+        <shop-image :width="500" :height="200" src="https://image.ibb2b.com/productcontent/2020/04/15/01e4072e-acfb-43c4-97c7-59c03ee8f0c2.jpg@0r_85Q" rounded></shop-image>
     </div>
 </template>
 
