ec3132b68c3f0d1a31850c0fae029b72ba0be316
chengxq <chengxq513@gmail.com>
2020-05-05 06:03:56 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 const defaultSettings = require('./src/settings.js')
 
 const path = require('path')
-const webname = defaultSettings.title || "minishop-default"
+const webname = defaultSettings.title || "MiniShop"
+
+const CompressionPlugin = require('compression-webpack-plugin')
 
 function resolve(dir) {
   return path.join(__dirname, dir)
@@ -18,12 +20,23 @@ module.exports = {
       }
     }
   },
-	configureWebpack: {
-    name:webname,
-    resolve: {
-     		alias: {
-       		'@': resolve('src')
-     		}
+	configureWebpack: (config) => {
+    config.name = webname
+    config.resolve.alias = {'@':resolve('src')}
+    if(process.env.NODE_ENV === 'production'){
+      config.mode = 'production'
+      if(openGzip){
+        config.plugins = [
+          ...config.plugins,
+          new CompressionPlugin({
+            test:/\.js$|\.html$|.\css/,
+            threshold:10240,
+            deleteOriginalAssets:false
+          })
+        ]
+      }
+    }else{
+      config.mode = 'development'
     }
   },
   productionSourceMap: false,
